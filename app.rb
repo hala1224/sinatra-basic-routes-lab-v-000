15adbc7f46607e7ab98a0a83647acb31fdec34bf
Natalie Halawani <hala1224@gmail.com>
2019-03-04 03:03:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "My name is Natasha "
+  end
+  get '/' do
+    "My hometown is Far Away "
+  end
+  get '/' do
+    "My favorite song : 'Stairway to Heaven' "
+  end
+  
+  
 end
